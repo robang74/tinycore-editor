2bababc4f41041950dec1af7cf22bd4c2ae1779b
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2021-08-20 14:44:08 +0200

    busybox-1.33.1-8of8-evalstring-uses-global-LINENO.patch added

---
+++ busybox/tests/test8.sh
@@ -0,0 +1,38 @@
+#!/bin/ash
+
+function realexit() {
+	trap - EXIT
+	exit $1
+}
+
+function perr() {
+	echo -e "\e[1;31m$@\e[0m"
+}
+
+function onerror() {
+	rc=$?
+#	echo
+	perr "ERROR: $(basename $0) failed${2+ in $2()} at line $1, rc: $rc" 
+	echo "$FUNCNAME() at line $LINENO == 16"
+#	echo
+#	realexit $rc
+}
+
+function mytest() {
+	echo "hello by $FUNCNAME() at line $LINENO == 22"
+	false
+	true
+}
+trap 'onerror $LINENO $FUNCNAME' ERR
+trap 'echo exit in $FUNCNAME\(\) at line $LINENO == 35 with pippo but not pluto' EXIT
+#exec 2>&1; set -x
+echo "ciao! at line $LINENO == 29"
+mytest
+echo "pippo"
+set -e
+
+echo LINENO=$LINENO
+eval 'echo LINENO=$LINENO; set -o invalid' 2>&1
+
+
+echo "pluto at line 38"
