2a4de53a4ea6400695a72eb077a97f20b8ca0fc1
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2021-08-21 17:29:23 +0200

    tcz2tce.sh refuses to tce if meta + improvements

--- tinycore/changes/tcz2tce.sh
+++ tinycore/changes/tcz2tce.sh
@@ -13,17 +13,37 @@ if [ "$USER" != "root" ]; then
 	exit 1
 fi
 
+tcdir=$(readlink /etc/sysconfig/tcdir)
+cd $tcdir
+
+for i in $(ls -1 tcz/*-meta.tcz 2>/dev/null); do
+	if [ "$1" == "back" -o "$1" == "reverse" ]; then
+		break;
+	else
+		echo
+		echo "The tce folder is not compliat with meta-package, abort"
+		echo
+		exit 1
+	fi
+	break
+done
+
 readonly=0
-mntdir=$(readlink /etc/sysconfig/tcdir)
-if grep -qe " $mntdir .* ro," /proc/mounts; then
-	mount -o remount,rw $mntdir
+if grep -qe " $tcdir .* ro," /proc/mounts; then
+	mount -o remount,rw $tcdir
+	if ! grep -qe " $tcdir .* rw," /proc/mounts; then
+		echo
+		echo "Cannot remount in read-write the $tcdir, abort"
+		echo
+		exit 1
+	fi
 	readonly=1
 fi
 optional="tce/optional"
-cd $mntdir
 
 if [ "$1" == "back" -o "$1" == "reverse" ]; then
 	if [ -d tce ]; then
+		echo
 		mkdir -p tcz
 		mv -f $optional/*.tcz* tcz
 		if [ "$1" == "reverse" ]; then
@@ -32,23 +52,28 @@ if [ "$1" == "back" -o "$1" == "reverse" ]; then
 			rm -f /etc/sysconfig/tcedir
 			ln -sf /tmp/tce /etc/sysconfig/tcedir
 		fi
+		ls -1 tcz/*.tcz 2>/dev/null
+		echo
 	fi
 	sync
 	if [ "$readonly" != "0" ]; then
-		mount -o remount,ro $mntdir
+		mount -o remount,ro $tcdir
 	fi
 	exit 0
 fi
 
+echo
 mkdir -p $optional
 ls -1 tcz/*.tcz 2>/dev/null && \
 	mv -f tcz/*.tcz* $optional
 cd $optional
 ls -1 *.tcz >../onboot.lst 2>/dev/null
 rm -f /etc/sysconfig/tcedir
-ln -sf $mntdir/tce /etc/sysconfig/tcedir
+ln -sf $tcdir/tce /etc/sysconfig/tcedir
 sync
+
 if [ "$readonly" != "0" ]; then
-	mount -o remount,ro $mntdir
+	mount -o remount,ro $tcdir
 fi
+echo
 
