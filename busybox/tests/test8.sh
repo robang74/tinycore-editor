9d48ef2550017a71a4bdd8e7af04a9fb377fdf19
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2021-08-23 14:05:38 +0200

    busybox test suite updated

--- busybox/tests/test8.sh
+++ busybox/tests/test8.sh
@@ -5,14 +5,14 @@ function realexit() {
 	exit $1
 }
 
-function perr() {
-	echo -e "\e[1;31m$@\e[0m"
+function setoinvalid() {
+	false
+	eval 'echo "$FUNCNAME LINENO=$LINENO == 10"; set -o invalid' 2>&1
 }
-
 function onerror() {
 	rc=$?
 #	echo
-	echo "ERROR: $(basename $0) failed${2+ in $2()} at line $1, rc: $rc" 
+	echo "ERROR: $(basename $0) failed${2+ in $2()} at line $1, rc: $rc != 0" 
 	echo "$FUNCNAME() at line $LINENO == 16"
 #	echo
 #	realexit $rc
@@ -24,15 +24,15 @@ function mytest() {
 	true
 }
 trap 'onerror $LINENO $FUNCNAME' ERR
-trap 'echo exit in $FUNCNAME\(\) at line $LINENO == 35 with pippo but not pluto' EXIT
+trap 'echo exit in $FUNCNAME\(\) at line $LINENO == 37 with pippo but not pluto' EXIT
 #exec 2>&1; set -x
 echo "ciao! at line $LINENO == 29"
 mytest
 echo "pippo, exit status $? == 0"
+
+echo "LINENO=$LINENO == 33, FUNCNAME='' == '$FUNCNAME'"
+setoinvalid
+echo "LINENO=$LINENO == 35, FUNCNAME='' == '$FUNCNAME'"
 set -e
-
-echo LINENO=$LINENO
 eval 'echo LINENO=$LINENO; set -o invalid' 2>&1
-
-
 echo "pluto at line 38"
