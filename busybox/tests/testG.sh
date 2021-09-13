6182bd2f0c161f37771d991aaab553050104b636
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2021-09-13 19:30:21 +0200

    busybox testG.sh expanded

--- busybox/tests/testG.sh
+++ busybox/tests/testG.sh
@@ -4,6 +4,22 @@ function myfalse() {
 	false
 }
 
+function myfault() {
+	command eval ")"
+}
+
+function myflase() {
+	flase
+}
+
+set 'echo "error in $FUNCANME at line $LINENO"' ERR
+exec 2>&1
+
+myfault
+echo "FUNCNAME='$FUNCNAME'"
+myflase
+echo "FUNCNAME='$FUNCNAME'"
+
 trap 'rc=$?; echo ERR at in $FUNCNAME at $LINENO, exit with $rc; exit $rc' ERR
 set -E
 myfalse
