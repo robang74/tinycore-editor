0faf2126c0f633448265e083260585ed8ea12cd8
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2024-10-01 17:18:44 +0200

    skeleton.sh: diskfatresize() fixed + chownuser & more
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- skeleton.sh
+++ skeleton.sh
@@ -4,15 +4,28 @@
 #
 
 function diskfatresize() {
-	local loop=$(losetup -f)
-	losetup -P $loop $1
-	trap "losetup -d $loop" EXIT
-	fatresize -i -n 1 $loop | grep size:
-	fatresize -vfs ${2}M -n 1 $loop
-	fatresize -i -n 1 $loop | grep size:
+	local loop size=$[$2/1024]
+	loop=$(losetup --show -Pf $1)
+	test -b $loop || exit 1
+	trap "losetp -d $loop 2>/dev/null" EXIT
+	fatresize -i -n 1 ${loop} | grep size:;	echo
+	fatresize -vfs ${size}k -n 1 ${loop}; echo
+	fatresize -i -n 1 ${loop} | grep "Cur size:"
 	losetup -d $loop
 	trap - EXIT
 }
+
+function chownuser() {
+	local user guid
+	user=$SUDO_USER
+	user=${user:-$USER}
+	guid=$(grep -e "^$user:" /etc/passwd | cut -d: -f3-4)
+	chown -R $guid "$@"
+}
+
+nrm="\x1b[0m"
+bld="\x1b[1m"
+red="\x1b[41m"
 
 function info() {
     echo -e "\e[1;36m$@\e[0m"
@@ -33,7 +46,12 @@ function perr() {
 set -e #########################################################################
 
 export myname=${myname:-$(basename $0)}
-export wrkdir=${wrkdir:-{$(dirname $0)}
+export wrkdir=${wrkdir:-$(dirname $0)}
+
+errmsg="\n${bld}>>> ${red}ERROR${nrm} in $myname"
+errmsg=$errmsg' at line $LINENO, abort.'$nrm'\n\n'
+trpcmd='eval "printf \"$errmsg\""'
+trap "$trpcmd" ERR
 
 if [ "$USER" != "root" ]; then
 	if ! timeout 0.2 sudo -n true; then
@@ -46,32 +64,49 @@ if [ "$USER" != "root" ]; then
 	exit $?
 fi
 
+if which pigz >/dev/null; then
+    gunzip() { pigz -d "$@"; }
+    zcat() { pigz -dc "$@"; }
+    gzip() { pigz "$@"; }
+fi
+
 size=${1:-256}
 disk=${size}MB.disk
 
-zcat tcl-skeleton.disk.gz >$disk
+skelzext="disk.gz"
+skelname="tcl-skeleton"
+skellink="$skelname.$skelzext"
+skelbase="${skelname}-35.${skelzext}"
+skelfile="${skelname}-${size}.${skelzext}"
 
+zcat $skelbase >$disk
 old_size=$(du -b $disk | cut -f1)
+
 new_size=$[size*1024*1000]
+disk_size=$[new_size+(2048*512)]
+dblk_size=$[((new_size+511)/512)+2048]
 
 if [ $new_size -lt $old_size ]; then
-	diskfatresize $disk $size
-	qemu-img resize --shrink -f raw $disk ${size}M
+	diskfatresize $disk $new_size
+	#qemu-img resize --shrink -f raw $disk $disk_size
+	dd if=/dev/zero seek=$dblk_size count=1 of=$disk
 elif [ $new_size -gt $old_size ]; then
-	qemu-img resize -f raw $disk ${size}M
-	echo -e "d\n n\n \n \n \n \n t\n b\n w\n" |\
-		sudo fdisk $disk
-	#RAF, TODO: this currently fails
-	diskfatresize $disk $size
+	#qemu-img resize -f raw $disk $disk_size
+	dd if=/dev/zero seek=$dblk_size count=1 of=$disk
+	echo -e "d\n n\n \n \n \n \n t\n b\n a\n w\n" |\
+	    tr -d ' ' | fdisk $disk
+	diskfatresize $disk $new_size
 fi
 
-gzip -9c $disk > tcl-skeleton-$size.disk.gz
-chown $SUDO_USER.$SUDO_USER tcl-skeleton-${size}.disk.gz
-ln -sf tcl-skeleton-${size}.disk.gz tcl-skeleton.disk.gz
-chown $SUDO_USER.$SUDO_USER -h tcl-skeleton.disk.gz
-ls -1l tcl-skeleton.disk.gz
-#gparted $loop
+gzip -9c $disk > ${skelfile}
+ln -sf ${skelfile} ${skellink}
+chownuser ${skellink} ${skelfile}
 rm -f $disk
+
+printf "link -> $skellink\n\n$bld"
+echo "Skeleton disk size: ${size} Mb"
+du -ks $skelfile | tr '\t' ' '
+printf "${nrm}\n"
 
 exit 0 ###################################################
 #
@@ -79,7 +114,7 @@ exit 0 ###################################################
 #
 ##########################################################
 
-#dd if=/dev/zero bs=1M count=$size of=$disk
+#dd if=/dev/zero bs=1M count=${size} of=$disk
 img_size=$[size*1024*1024]
 # bytes per sector
 bytes=512
@@ -109,9 +144,9 @@ syslinux -H $heads -S $sectors -d /boot/syslinux -i ${loop}p1 || true
 zcat tcl-usb-boot-enable.gz >$loop
 sync 
 losetup -d $loop
-gzip -9c $disk >tcl-skeleton-${size}.disk.gz
-ln -sf tcl-skeleton-${size}.disk.gz tcl-skeleton.disk.gz
-chown $SUDO_USER.$SUDO_USER tcl-skeleton-${size}.disk.gz
-chown $SUDO_USER.$SUDO_USER -h tcl-skeleton.disk.gz
-ls -1l tcl-skeleton.disk.gz
+gzip -9c $disk >${skelname}-${size}.${skelzext}
+ln -sf ${skelname}-${size}.${skelzext} ${skelname}.${skelzext}
+chown $SUDO_USER.$SUDO_USER ${skelname}-${size}.${skelzext}
+chown $SUDO_USER.$SUDO_USER -h ${skelname}.${skelzext}
+ls -1l ${skelname}.${skelzext}
 rm -f $disk
