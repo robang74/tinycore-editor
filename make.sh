48d6b808575c615ec9edbb05aa1d4de6b20b81b4
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2024-10-01 02:18:06 +0200

    tcl-skeleton-${size}.disk.gz with size in {64,128,256} updated
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- make.sh
+++ make.sh
@@ -720,10 +720,7 @@ if [ "$param" == "image" -a "$option" != "8GB" ]; then
 	storage_32GB_create
 	if [ ! -e tcl-skeleton.disk.gz ]; then
 		ln -sf tcl-skeleton-128.disk.gz tcl-skeleton.disk.gz
-<<<<<<< HEAD
 		chownuser tcl-skeleton.disk.gz
-=======
->>>>>>> 6d01d01 (skeleton.sh: rewritten entirely, rationalised)
 	fi
 	zcat tcl-skeleton.disk.gz >tcl-usb.disk
 	sync
